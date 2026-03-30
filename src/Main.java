f57a01a9277cfb149851a05dcb624fb5bc067903
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-03-30 17:37:52 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 
 // Operador booleano e de igual ==/ e essa operação está comparando o resultado
 // que o usuário escrever para o resultado que é esperado
+
 public class Main {
     public static void main(String[] args) {
         var scanner =  new Scanner(System.in);
-        System.out.printf("Quanto é 2 + 2?");
+        /*    System.out.printf("Quanto é 2 + 2?");
         var result = scanner.nextInt();
         var isRight = result == 4;
         System.out.printf("O resultado é 4, você acertou? (%s)", result == 4);
+        */
+
+        System.out.printf("Quantos anos você tem?");
+        var age = scanner.nextInt();
+        var canNotDrive = age <= 17;
+        System.out.printf("Você  pode dirigir? (%s) \n", !canNotDrive );
     }
 }
+
+// também pode fazer assim:
+// var canDrive = !(age < 18);
